541c191a82589f52492e71e570a94ed862e521f7
Muhammed Shanid <shanishanid91@gmail.com>
2023-03-04 06:33:10 +0530

    dummy transactions created

---
+++ src/data/transactions.js
@@ -0,0 +1,23 @@
+export const transactions = [
+    {
+        to:"Aokiji",
+        amount:13.00,
+        type:"Received",
+        date:"28 Feb 2023",
+        time:"06:23 PM"
+    },
+    {
+        to:"Kizaru",
+        amount:9.00,
+        type:"Outgoing",
+        date:"28 Feb 2023",
+        time:"06:23 PM"
+    },
+    {
+        to:"Akainu",
+        amount:20.00,
+        type:"Received",
+        date:"28 Feb 2023",
+        time:"06:23 PM"
+    },
+]
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TransactionList from '../../components/transactions/TransactionList'
+
+const Home = () => {
+  return (
+    <div className='bg-gray-100 '>
+        <TransactionList/>
+    </div>
+  )
+}
+
+export default Home
