4ad7e44e0907bd52b1ffafc2b0ad46fde919d955
Muhammed Shanid <shanishanid91@gmail.com>
2023-03-04 11:40:18 +0530

    final commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,7 +4,7 @@ import TransactionList from '../../components/transactions/TransactionList'
 const Home = () => {
   return (
     <div className='bg-gray-100 w-full min-h-screen md:py-10 md:px-20 lg:py-14 lg:px-[10rem]
-    xl:py-[5rem] xl:px-[15rem] sm:py-6 sm:px-10  flex items-center justify-center'>
+    xl:py-[5rem] xl:px-[15rem] sm:py-6 sm:px-10 px-6 flex items-center justify-center'>
         <TransactionList/>
     </div>
   )
